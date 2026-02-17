195d2c72b8cb19d37d5585df08f7b0cd698a7449
Nick Nissen <nickknissen@gmail.com>
2026-02-17 07:56:21 +0100

    chore: bump api version

--- src/aula/const.py
+++ src/aula/const.py
@@ -1,6 +1,6 @@
 DOMAIN = "aula"
 API_URL = "https://www.aula.dk/api/v"
-API_VERSION = "21"
+API_VERSION = "22"
 MIN_UDDANNELSE_API = "https://api.minuddannelse.net/aula"
 SYSTEMATIC_API = "https://systematic-momo.dk/api/aula"
 EASYIQ_API = "https://api.easyiqcloud.dk/api/aula"
